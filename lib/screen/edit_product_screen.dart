b18508d446cb9d870bbc3efb702e0974e731abb3
bawar <bawarkhalid912@gmail.com>
2022-08-13 07:33:49 +0300

    added form to input user

---
+++ lib/screen/edit_product_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class EditProductScreen extends StatefulWidget {
+  const EditProductScreen({Key? key}) : super(key: key);
+
+  @override
+  State<EditProductScreen> createState() => _EditProductScreenState();
+}
+
+class _EditProductScreenState extends State<EditProductScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Edit Product",
+        ),
+      ),
+      body: Form(
+        child: ListView(
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Title',
+              ),
+              textInputAction: TextInputAction.next,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
